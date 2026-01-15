0ec620edf9f2a0fe05bbf7ac0527ca189cde8a4a
Aidan Lenahan <d9hhhh@gmail.com>
2026-01-15 09:17:07 -0500

    Update email SMTP configuration credentials

--- includes/email_config.php
+++ includes/email_config.php
@@ -2,14 +2,13 @@
 /**
  * Email Configuration
  * SMTP credentials for sending emails
- Don't try to use these credentials, they have been cycled ;)
  */
 
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 define('SMTP_SECURE', 'tls');
-define('SMTP_USERNAME', 'f3fff0@gmail.com');
-define('SMTP_PASSWORD', 'ryge ebal lzyh wvxo');
-define('SMTP_FROM_EMAIL', 'f3fff0@gmail.com');
-define('SMTP_FROM_NAME', 'BUCIT Support');
+define('SMTP_USERNAME', 'user@gmail.com');
+define('SMTP_PASSWORD', 'xxxx xxxx xxxx xxxx');
+define('SMTP_FROM_EMAIL', 'user@gmail.com');
+define('SMTP_FROM_NAME', 'Sender Name');
 ?>
